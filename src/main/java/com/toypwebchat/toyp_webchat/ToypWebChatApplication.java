c8b8dbd4643967c3d9b52a94172227b4b3c27c93
kimyk0120 <kimyk0120@hotmail.com>
2022-12-28 18:10:06 +0900

    init

---
+++ src/main/java/com/toypwebchat/toyp_webchat/ToypWebChatApplication.java
@@ -0,0 +1,13 @@
+package com.toypwebchat.toyp_webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToypWebChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToypWebChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toypwebchat/toyp_webchat/ToypWebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toypwebchat.toyp_webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToypWebChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

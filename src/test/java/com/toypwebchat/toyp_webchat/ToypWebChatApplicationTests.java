21e31d6755eb63f9ff79ce1c202c0a0447dc71b7
kimyk0120 <kimyk0120@hotmail.com>
2023-01-09 16:27:09 +0900

    * 단위 테스트 init

--- src/test/java/com/toypwebchat/toyp_webchat/ToypWebChatApplicationTests.java
+++ src/test/java/com/toypwebchat/toyp_webchat/ToypWebChatApplicationTests.java
@@ -5,12 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 
 @Slf4j
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application.properties")
 class ToypWebChatApplicationTests {
 
 

f13f1d1d1cd49d4bb0605e7f212c84162c4058d0
kimyk0120 <kimyk0120@hotmail.com>
2023-01-11 18:34:01 +0900

    * mod github action

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
          echo "> no ing app."
  else
          echo "> kill -9 $CURRENT_PID"
-         kill -9 $CURRENT_PID
+         kill -9 "$CURRENT_PID"
          sleep 3
  fi
  echo "> new app deploy"
@@ -17,5 +17,6 @@
  JAR_NAME=$(ls | grep 'Toyp_WebChat' | tail -n 1)
  echo "> JAR Name: $JAR_NAME"
 
- nohup java -jar -Duser.timezone=Asia/Seoul "$JAR_NAME" 1>nohup/stdout.txt 2>nohup/stderr.txt &
+ # shellcheck disable=SC2086
+ nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME 1>nohup/stdout.txt 2>nohup/stderr.txt &
  sleep 2

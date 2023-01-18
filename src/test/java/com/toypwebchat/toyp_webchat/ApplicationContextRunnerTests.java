b8c644b7190f95d550fd6582d37e3795d200d2e6
kimyk0120 <kimyk0120@hotmail.com>
2023-01-18 18:11:32 +0900

    * add login validation

--- src/test/java/com/toypwebchat/toyp_webchat/ApplicationContextRunnerTests.java
+++ src/test/java/com/toypwebchat/toyp_webchat/ApplicationContextRunnerTests.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.event.RecordApplicationEvents;
 
@@ -14,6 +17,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
+@EnableAsync
 @Slf4j
 @SpringBootTest
 @RecordApplicationEvents
@@ -24,13 +28,16 @@ class ApplicationContextRunnerTests {
     @Test
     public void happyPathSuccess() {
         AtomicBoolean sideEffectCausedByEvent = new AtomicBoolean(false);
+
         ObservableEffect effect = () -> sideEffectCausedByEvent.set(true);
 
         runner.withBean(SomeEventListener.class, effect)
                 .run(
                     context -> {
-                        context.publishEvent(new SomeEvent());
-                        log.info("sideEffectCausedByEvent is : " + sideEffectCausedByEvent);
+                        log.info("before sideEffectCausedByEvent is : " + sideEffectCausedByEvent);
+                        context.publishEvent(new SomeEvent("test publish event"));
+                        Thread.sleep(3000);
+                        log.info("after sideEffectCausedByEvent is : " + sideEffectCausedByEvent);
                         assertThat(sideEffectCausedByEvent.get()).isTrue();
                     });
     }
@@ -48,12 +55,24 @@ class ApplicationContextRunnerTests {
             this.effect = effect;
         }
 
+        @Async
         @EventListener(SomeEvent.class)
-        public void listen() {
+        public void listen(SomeEvent event) {
             effect.effect();
+            log.info("#listen event is : " + event.getMessage());
         }
     }
 
-    public static class SomeEvent {}
+    public static class SomeEvent extends ApplicationEvent {
+        private final String message;
+        public SomeEvent(Object source) {
+            super(source);
+            this.message = (String) source;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
 
 }
